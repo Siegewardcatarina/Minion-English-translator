a8d8962b9c989957f51f6ecf873373395ca499d6
Supriyo Dey <deysupriyo041@gmail.com>
2022-09-11 21:05:51 +0530

    docs: added id

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-var btn = document.querySelector("#btn-translate");
-btn.addEventListener("click",function click(){
+// var btns = document.querySelectorAll(".bttn");
+// for(let btn of btns) {
+//     btn.addEventListener("click",function click(){
+//         console.log("clicked");
+//     })
+// }
+
+const bodyParent = document.querySelector("#bodyParent")
+bodyParent.addEventListener("click",function click(){
     console.log("clicked");
 })
