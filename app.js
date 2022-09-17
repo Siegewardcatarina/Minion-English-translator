fb23ff90a10b6be6a82b2622f08c88170ffe694b
Supriyo Dey <deysupriyo041@gmail.com>
2022-09-17 17:10:25 +0530

    feat: added ability to take input from user

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-var txt = document.querySelector("#btn-primary");
-txt.addEventListener("click",clickHandler{
-    console.log("click!");
-})
+var btn = document.querySelector("#btn-primary");
+var txt = document.querySelector("#txt-input");
+
+function clickeventhandler() {
+    console.log("clicked!"); 
+    console.log("input", txt.value);
+ }
+
+btn.addEventListener("click",clickeventhandler);
