9bf86f0663272a90a7a29531dfa6e35159c11b30
Supriyo Dey <deysupriyo041@gmail.com>
2022-09-17 19:55:15 +0530

    Calling server in app

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ var btn = document.querySelector("#btn-primary");
 var txt = document.querySelector("#txt-input");
 var out = document.querySelector("#output");
 
-function clickeventhandler() {
-    out.innerText = "Translated " + txt.value;
+var serverurl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function geturl(text)
+{
+    return serverurl + '?' + 'text=' + text;
 }
 
+function clickeventhandler(){
+    var inputtxt = txt.value;
+    fetch(geturl(inputtxt))
+        .then(response => response.json())
+        .then(json => console.log(json))
+}
 btn.addEventListener("click",clickeventhandler);
